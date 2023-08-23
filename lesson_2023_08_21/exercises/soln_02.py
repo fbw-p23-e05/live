fecf2d9355439fd764edc07bf75a585c3787c3e5
Kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2023-08-23 15:28:48 +0200

    add solutions

---
+++ lesson_2023_08_21/exercises/soln_02.py
@@ -0,0 +1,71 @@
+
+
+# Task1
+# Convert a decimal number to binary
+
+# num = 27
+# binary = bin(num)
+# print(binary) # 0b11011
+
+
+# Task2
+# Convert a binary number to decimal
+
+# num = 0b11011
+# decimal = int(num)
+# print(decimal) # 27
+
+
+# Task3
+# Convert a decimal number to hexadecimal
+
+# num = 27
+# hexadecimal = hex(num)
+# print(hexadecimal) # 0x1b
+
+
+# Task4
+# Convert a hexadecimal number to decimal
+
+# num = 0x1b
+# decimal = int(num)
+# print(decimal) # 27
+
+
+
+# Task5
+# Convert a binary number to hexadecimal
+
+# num_binary = 0b11011
+# hexadecimal = hex(num_binary)
+# print(hexadecimal) # 0x1b
+
+
+# Task6
+# Convert a hexadecimal number to binary
+
+# num_hexadecimal = 0x1b
+# num_binary = bin(num_hexadecimal)
+# print(num_binary) # 0b11011
+
+
+
+### Task 7
+# Convert a decimal number to octal
+
+# num = 27
+# num_octal = oct(num)
+# print(num_octal) # 0o33
+
+
+
+### Task 8
+# Convert a octal number to decimal
+
+num_octal = 0o33
+
+num_decimal = int(num_octal)
+
+print(num_decimal) #27
+
+
