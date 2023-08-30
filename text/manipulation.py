4828818b3ac693509cf321243ecf553c5c11e4fb
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-08-30 13:46:11 +0200

    Text introduction

---
+++ text/manipulation.py
@@ -0,0 +1,63 @@
+# my_city = "Kigali"
+
+# # We can also use double quotes
+# my_city = 'Kigali'
+
+# print(type(my_city))
+
+# # Setting a variable explicitly
+# my_num = str(1500)
+# my_name = str('Carlos')
+
+# print(type(my_num))
+# print('My name is a:', type(my_name))
+
+# Concatenate Strings
+
+# first_word = 'Programming'
+# second_word = 'Basics'
+
+# full_word = first_word + second_word
+
+# print(full_word)
+# print(first_word + ' ' + second_word)
+
+# Getting the size of a string
+
+# size = len('Size of a String')
+# print(size)
+
+# phrase = 'Do not repeat yourself!'
+
+# num_of_chars = len(phrase)
+
+# for char in range(num_of_chars):
+#     print(char)
+    
+# for char in phrase:
+#     print(char)
+
+
+# Counting in a string
+# my_sentence = "The quick brown fox jumps over the lazy dog."
+
+# print(my_sentence.count('the'))
+
+# Splitting a string
+# my_phrase = 'We should have fun in the sun!'
+
+# # By default it will split using spaces
+# my_words = my_phrase.split('fun')
+
+# print(my_phrase.split('fun'))
+
+# for word in my_words:
+#     print(word)
+
+
+# Replacing a char or text from a string
+
+my_sentence = "The quick brown fox jumps over the lazy dog."
+
+print(my_sentence.replace('o', ''))
+
