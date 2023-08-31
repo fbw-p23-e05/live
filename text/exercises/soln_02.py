ee93001f5560b24739ceadc9a65f4e73885fb2f7
Kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2023-08-31 13:45:08 +0200

    add solutions

---
+++ text/exercises/soln_02.py
@@ -0,0 +1,64 @@
+
+# Task 1
+
+# text = "Berlin is a world city of culture, politics, media and science."
+
+# print(len(text))
+
+
+# Task 2
+
+# text = "Berlin is a world city of culture, politics, media and science."
+
+# print(text[0], text[-1])
+
+
+# Task 3
+
+# text = "Berlin is a world city of culture, politics, media and science."
+
+# # text_upper = text.upper()
+# # print(text_upper)
+
+# # print(text_upper[0:3])
+
+# print(text[:3].upper())
+
+
+# Task 4
+
+# text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"
+
+
+# print('B appears:',  text.count('B'), 'times')
+
+
+# Task 5
+
+# text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+
+# print(f'Last ten characters: {text[-10:]}')
+
+
+# Task 6
+
+
+# text = '---Python -- programming---'
+
+# # print(text[:9] + ' ' +  text[13:])
+
+# # print(f'{text[0:9]} {text[13:]}')
+
+# # print(text.strip('-'))
+
+# print(text.lstrip('-').rstrip('-'))
+
+
+# Task 7
+
+first_name = 'Jansen'
+last_name = 'Mansen'
+
+print(f'Firstname: {first_name}\nLastname: ' + f'{last_name}')
+
+# print('Firstname: ' + first_name + '\nLastname: ' + last_name)
