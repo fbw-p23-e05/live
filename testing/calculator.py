4bbdaaecadb713b7aac17d72d232677ddfee6230
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-11-21 12:29:34 +0200

    Testing exceptions.

--- testing/calculator.py
+++ testing/calculator.py
@@ -2,17 +2,38 @@ class Calculator:
     
     # Add
     def add(self, x, y):
+        # Check if both variables are integers
+        if not isinstance(x, int) or not isinstance(y, int):
+            # Else: raise exception
+            raise TypeError 
+          
+        # return the sum
         return x + y
     
     # Subtract
     def subtract(self, x, y):
-        return x - y
+        if isinstance(x, int) and isinstance(y, int):
+            return x - y
+        
+        raise TypeError("Both values are supposed to be integers")
     
     # Multiply
     def multiply(self, x, y):
-        return x * y
+        if isinstance(x, int) and isinstance(y, int):
+            return x * y
+        raise TypeError
     
     # Divide
     def divide(self, x, y):
+        
+        # Check if either of the values are not integers
+        if not isinstance(x, int) or not isinstance(y, int):
+            # raise TypeError
+            raise TypeError("Both values must be integers.")
+        
+        # Check if y is equal to zero
+        elif y == 0:
+            # raise ZeroDivision Error
+            raise ZeroDivisionError("You cannot divide by 0.")
+        
         return x / y
-    
--- testing/tests/test_calculator.py
+++ testing/tests/test_calculator.py
@@ -21,33 +21,60 @@ class TestCalculator(unittest.TestCase):
         print("I was executed.")
         
     # Add
-    def test_add(self):
+    def test_add_returns_correct_result(self):
         """
         Test for add method. Adds the 2 parameters expecting an integer result.
         """
-        self.b = 15
         result = self.calc.add(self.a, self.b)
-        self.assertTrue(result == 35)
-        self.assertEqual(type(result), type(35))
+        self.assertTrue(result == 30)  # correct result
+         
+    def test_add_returns_integer(self):
+        result = self.calc.add(self.a, self.b)
+        self.assertEqual(type(result), type(35))  #  Correct data type on result
+        
+    def test_add_raises_type_error(self):
+        """
+        Tests if add function raises Type Error if either of values is not an integer.
+        """
+        self.assertRaises(TypeError, self.calc.add, 10, "19")
     
     # Subtract
     def test_subtract(self):
         result = self.calc.subtract(self.a, self.b)
         self.assertTrue(result == 10)
         self.assertEqual(type(result), type(10))
+        
+    def test_subtract_raises_type_error(self):
+        self.assertRaises(TypeError, self.calc.subtract, 10, "7")
     
     # Multiply
     def test_multiply(self):
         result = self.calc.multiply(self.a, self.b)
         self.assertEqual(result, 200)
         self.assertEqual(type(result), type(200))
+        
+    def test_multiply_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.calc.multiply(self.a, 12.5)
+            self.calc.multiply(13, True)
+            self.calc.multiply(13, "14")
     
     # Divide
     def test_divide(self):
         result = self.calc.divide(self.a, self.b)
         self.assertEqual(result, 2)
         self.assertEqual(type(result), type(2.0), msg="Expected result is a integer.")
-
+        
+    def test_divide_raises_zero_division_error(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(15, 0)
+            self.calc.divide(20, 0)
+            
+    def test_divide_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            self.calc.divide("12", 19)
+            self.calc.divide(True, 12)
+            self.calc.divide(12.5, 10.4)
   
 if __name__ == '__main__':
     unittest.main()
