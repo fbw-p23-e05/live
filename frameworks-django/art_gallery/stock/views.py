a860f08ab5d27b6b72238ff63d3273179520a1fb
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-02-02 11:25:40 +0200

    URL Conf

--- frameworks-django/art_gallery/stock/views.py
+++ frameworks-django/art_gallery/stock/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello World!")
+
+
+def listings(request):
+    return HttpResponse("<ol><li>Maksym</li><li>Monica</li><li>Shilpa</li></ol>")
