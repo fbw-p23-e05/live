336d04230ccb57e9ed8a20751294c52043ecd6da
Mason Mapfundematsva <masonmapfunde@gmail.com>
2024-02-02 13:06:33 +0200

    Keyword params

--- frameworks-django/art_gallery/stock/views.py
+++ frameworks-django/art_gallery/stock/views.py
@@ -8,3 +8,21 @@ def home(request):
 
 def listings(request):
     return HttpResponse("<ol><li>Maksym</li><li>Monica</li><li>Shilpa</li></ol>")
+
+
+def search_item(request, item_id):
+    items = ["Laptop", "Android Phone", "Keyboard", "Mouse"]
+    next_item = item_id + 1
+    print(next_item)
+    item = items[item_id]
+    return HttpResponse(f"You requested the item {item}.")  
+
+
+def search_item_by_key(request, item_name):
+    items = {
+        "Phones": "We have some android phones in stock",
+        "Laptops": "We also sell high-end laptops",
+        "Monitors": "Top of the line monitors available.",
+    }
+    item = items[item_name]
+    return HttpResponse(item)
