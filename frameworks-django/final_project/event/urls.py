b1278acdf44d5e55e973a9125482e45ecdc4a43b
Kamrujjaman Md <kamrujjaman.md@digitalcareerinstitute.org>
2024-03-21 15:54:50 +0100

    consume basic api

--- frameworks-django/final_project/event/urls.py
+++ frameworks-django/final_project/event/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from . import api_views
+from . import views, api_views
 
 app_name = 'event'
 
 urlpatterns = [
     path('events/', api_views.EventListCreate.as_view(), name='event-list-create'),
     path('events/<int:pk>/', api_views.EventRetrieveUpdateDestroy.as_view(), name='event-retrieve-update-destroy'),
+
+    path('events/consume/', views.api_consume, name='api_consume'),
+
+
 ]
--- frameworks-django/final_project/event/views.py
+++ frameworks-django/final_project/event/views.py
@@ -1,3 +1,22 @@
+from django.shortcuts import render
+import requests
+
+def api_consume(request):
+
+    url = "https://jsonplaceholder.typicode.com/posts/"
+    response = requests.get(url)
+
+    url2 = "http://127.0.0.1:8000/api/events/"
+    response2 = requests.get(url2)
+    events = response2.json()[:5]
+
+    if response.status_code == 200:
+        data = response.json()[:5]
+    else:
+        print("Error:", response.status_code)
+
+    return render(request, 'event/api_event.html', {'data': data, 'events': events})
+
 
 
 
